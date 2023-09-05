e1a621e9ecaa51447364575769b0caa292300504
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-06 06:28:54 +0800

    useState Object

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,25 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState('Jeff');
+  const [age, setAge] = useState(26);
+  const [hobby, setHobby] = useState('listening to music');
+
+  const displayPerson = () => {
+    setName('Marc');
+    setAge('24');
+    setHobby('Playing Piano');
+  };
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h4>Enjoy: {hobby}</h4>
+      <button type="button" className="btn" onClick={displayPerson}>
+        Show Marc
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
