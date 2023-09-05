e51fa0ee11c11040c2c0074be79e798c288a63f9
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-06 06:15:45 +0800

    useState array

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+import { data } from '../../../data';
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [users, setUsers] = useState(data);
+
+  const removeUser = (name) => {
+    const newUsers = users.filter((user) => user.name !== name);
+    setUsers(newUsers);
+  };
+
+  const clearUsers = () => {
+    setUsers([]);
+  };
+  return (
+    <div>
+      <h2>useState array example</h2>
+      <ul>
+        {users.map((user) => {
+          return (
+            <div key={user.id}>
+              <li>{user.name}</li>
+              <button type="button" onClick={() => removeUser(user.name)}>
+                remove
+              </button>
+            </div>
+          );
+        })}
+      </ul>
+      <button
+        type="button"
+        onClick={clearUsers}
+        className="btn"
+        style={{ marginTop: '2rem' }}
+      >
+        Clear Users
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
