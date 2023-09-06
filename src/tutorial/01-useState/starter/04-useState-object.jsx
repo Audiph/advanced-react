1811d9f4a03081017765245b2ab6d4eda5c9c2e1
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-06 18:28:23 +0800

    refactor useState Object

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,20 +1,30 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  const [name, setName] = useState('Jeff');
-  const [age, setAge] = useState(26);
-  const [hobby, setHobby] = useState('listening to music');
+  const [person, setPerson] = useState({
+    name: 'Jeff',
+    age: 26,
+    hobby: 'listening to music',
+  });
+  // const [name, setName] = useState('Jeff');
+  // const [age, setAge] = useState(26);
+  // const [hobby, setHobby] = useState('listening to music');
 
   const displayPerson = () => {
-    setName('Marc');
-    setAge('24');
-    setHobby('Playing Piano');
+    // setName('Marc');
+    // setAge('24');
+    // setHobby('Playing Piano');
+    setPerson({
+      name: 'Marc',
+      age: 24,
+      hobby: 'playing piano',
+    });
   };
   return (
     <>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h4>Enjoy: {hobby}</h4>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Enjoy: {person.hobby}</h4>
       <button type="button" className="btn" onClick={displayPerson}>
         Show Marc
       </button>
