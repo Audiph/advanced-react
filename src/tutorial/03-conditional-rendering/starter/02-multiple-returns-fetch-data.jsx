fb9b8fa4985136631824e21baeef96cd6da45096
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-08 04:04:33 +0800

    add isLoading and isError and remove destructuring object

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -11,24 +13,27 @@ const MultipleReturnsFetchData = () => {
         setUser(newUser);
       } catch (error) {
         console.log(error);
+        setIsError(true);
       }
+      setIsLoading(false);
     };
     fetchData();
   }, []);
 
-  if (user === null) return <h2>Loading...</h2>;
+  if (isLoading) return <h2>Loading...</h2>;
 
-  const { avatar_url, name, company, bio } = user;
+  if (isError) return <h2>There was an error...</h2>;
+
   return (
     <div>
       <img
-        src={avatar_url}
-        alt={name}
+        src={user.avatar_url}
+        alt={user.name}
         style={{ width: '150px', borderRadius: '25px' }}
       />
-      <h2>{name}</h2>
-      <h4>works at {company}</h4>
-      <p>{bio}</p>
+      <h2>{user.name}</h2>
+      <h4>works at {user.company}</h4>
+      <p>{user.bio}</p>
     </div>
   );
 };
