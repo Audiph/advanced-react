87210ffdfb1c305f3f6b8212b6290e829c8d0c3f
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-07 21:39:32 +0800

    useEffect basics

--- src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
@@ -6,12 +6,16 @@ const UseEffectBasics = () => {
     console.log('hello there');
   };
 
+  useEffect(() => {
+    console.log('hello world!');
+  }, []);
+
   sayHello();
 
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
