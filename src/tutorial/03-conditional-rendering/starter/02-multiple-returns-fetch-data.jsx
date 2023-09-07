fbc490c3a695920b32ff8b76d5715a265a65b65a
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-08 03:55:15 +0800

    multiple returns rendering

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,34 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const newUser = await fetch(url).then((res) => res.json());
+        setUser(newUser);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (user === null) return <h2>Loading...</h2>;
+
+  const { avatar_url, name, company, bio } = user;
+  return (
+    <div>
+      <img
+        src={avatar_url}
+        alt={name}
+        style={{ width: '150px', borderRadius: '25px' }}
+      />
+      <h2>{name}</h2>
+      <h4>works at {company}</h4>
+      <p>{bio}</p>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
