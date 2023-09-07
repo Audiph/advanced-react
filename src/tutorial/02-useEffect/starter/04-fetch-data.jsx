986035ffb8a01a0c516a64d3d6b59da7e4fd8290
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-08 02:53:00 +0800

    useEffect fetch data from external API

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from 'react';
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const list = await fetch(url).then((res) => res.json());
+      setUsers(list);
+    };
+    fetchData();
+  }, [users]);
+
+  return (
+    <section>
+      <h3>github users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { login, url, avatar_url, id } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h5>{login}</h5>
+                <a href={url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 };
 export default FetchData;
