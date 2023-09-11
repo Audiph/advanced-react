6976fb54988a431d1535496239badaf651ae9061
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-12 06:27:04 +0800

    cleanup useEffect functions

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 const CleanupFunction = () => {
   const [toggle, setToggle] = useState(false);
-
+  console.log('render');
   return (
     <div>
       <button className="btn" onClick={() => setToggle(!toggle)}>
@@ -15,7 +15,11 @@ const CleanupFunction = () => {
 
 const RandomComponent = () => {
   useEffect(() => {
-    console.log('hmm this is interesting');
+    const intId = setInterval(() => {}, 1000);
+    return () => {
+      clearInterval(intId);
+      console.log('cleanup');
+    };
   }, []);
   return <h1>hello there</h1>;
 };
