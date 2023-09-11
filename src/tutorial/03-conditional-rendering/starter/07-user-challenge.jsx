0560d559dc739d3f81cfabacf5e37c88d3a0fe84
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-12 06:14:07 +0800

    user challenge

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    setUser({ name: 'Jeff' });
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <div>
+      {user ? (
+        <>
+          <h4>hello there, {user.name}</h4>
+          <button className="btn" onClick={logout}>
+            logout
+          </button>
+        </>
+      ) : (
+        <>
+          <h4>please login</h4>
+          <button className="btn" onClick={login}>
+            login
+          </button>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
