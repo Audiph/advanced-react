21fe40d8747aa43b814af00d573c62617b7a846f
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-02 18:04:02 +0800

    other inputs - dropdown select

--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react';
 const frameworks = ['react', 'angular', 'vue', 'svelte'];
 const OtherInputs = () => {
   const [shipping, setShipping] = useState(false);
+  const [framework, setFramework] = useState('react');
 
   const handleShipping = (e) => {
     console.log(e.target.checked);
     setShipping(e.target.checked);
+  };
+
+  const handleFramework = (e) => {
+    setFramework(e.target.value);
   };
   return (
     <div>
@@ -26,6 +31,20 @@ const OtherInputs = () => {
           <label htmlFor="framework" className="form-label">
             Framework
           </label>
+          <select
+            name="framework"
+            id="framework"
+            value={framework}
+            onChange={handleFramework}
+          >
+            {frameworks.map((item) => {
+              return (
+                <option value={item} key={item}>
+                  {item}
+                </option>
+              );
+            })}
+          </select>
         </div>
         <button type="submit" className="btn btn-block">
           submit
