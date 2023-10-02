c1efbcb0e6544e9daf864fd1c1bed88341b25b76
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-02 09:24:37 +0800

    leverage js complete

--- src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import avatar from '../../../assets/default-avatar.svg';
 
-const Person = ({ name, nickName, images }) => {
+const Person = ({ name, nickName = 'boi', images }) => {
+  const img = images?.[0]?.small.url || avatar;
   return (
     <div>
-      <img src={images[0].small.url} alt={name} style={{ width: '50px' }} />
+      <img src={img} alt={name} style={{ width: '50px' }} />
       <h4>{name}</h4>
       <p>Nickname: {nickName}</p>
     </div>
