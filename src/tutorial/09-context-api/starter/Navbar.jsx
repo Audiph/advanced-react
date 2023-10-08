2902493f98290cc863ba31f7123e45c55e30b291
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-09 01:41:41 +0800

    prop drilling

---
+++ src/tutorial/09-context-api/starter/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import NavLinks from './NavLinks';
+
+const Navbar = () => {
+  const [user, setUser] = useState({ name: 'bob' });
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <div className="navbar">
+      <h5>CONTEXT API</h5>
+      <NavLinks user={user} logout={logout} />
+    </div>
+  );
+};
+
+export default Navbar;
